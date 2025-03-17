10fa5bae3c02e58a5980d0e229ddbdd7b51020e1
Quietguest <dkf595915002@gmail.com>
2025-03-17 19:45:03 -0400

    Add files via upload

--- front-end/src/pages/HomePage.tsx
+++ front-end/src/pages/HomePage.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import DateDisplay from '../components/DateDisplay'
+import Map from '../components/Map';
 
 const HomePage: React.FC = () => {
+    const defaultCenter = { lat: 40.7128, lng: -74.0060 };
+    const defaultZoom = 11;
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <h1 style={{ fontSize: '4em' }}>Hello world!</h1>
+            <h1>NYC Transit Map</h1>
+            <Map center={defaultCenter} zoom={defaultZoom} />
             <DateDisplay />
         </div>
     )
